c63bf0c12f74dce217ab756f8a81e09b62ac0463
javaansehz <javaansehz@gmail.com>
2019-03-03 22:43:33 +0100

    presentation

---
+++ docs/includes/slides/01-kubernetes/05-deployment.js
@@ -0,0 +1,21 @@
+var canvasDeployment1 = document.getElementById('canvasDeployment1');
+rectangle(canvasDeployment1, 10, 10, 380, 50, green, 'Deployment', 'black');
+
+rectangle(canvasDeployment1, 10, 140, 180, 300, burgundy, null, null);
+rectangle(canvasDeployment1, 10, 100, 60, 40, burgundy, 'Node', 'black');
+
+rectangle(canvasDeployment1, 30, 190, 140, 90, blue, null, null);
+rectangle(canvasDeployment1, 30, 160, 60, 30, blue, 'Pod', 'black');
+rectangle(canvasDeployment1, 40, 200, 120, 30, purple, 'Container', 'black');
+rectangle(canvasDeployment1, 40, 240, 120, 30, purple, 'Container', 'black');
+
+rectangle(canvasDeployment1, 30, 330, 140, 90, blue, null, null);
+rectangle(canvasDeployment1, 30, 300, 60, 30, blue, 'Pod', 'black');
+rectangle(canvasDeployment1, 40, 340, 120, 30, purple, 'Container', 'black');
+
+rectangle(canvasDeployment1, 210, 140, 180, 300, burgundy, null, null);
+rectangle(canvasDeployment1, 210, 100, 60, 40, burgundy, 'Node', 'black');
+
+rectangle(canvasDeployment1, 230, 190, 140, 90, blue, null, null);
+rectangle(canvasDeployment1, 230, 160, 60, 30, blue, 'Pod', 'black');
+rectangle(canvasDeployment1, 240, 200, 120, 30, purple, 'Container', 'black');
