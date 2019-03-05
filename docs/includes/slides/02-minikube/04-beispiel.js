6baabf6df6bd29b8e4c529ec35401cf3a5c39a8c
javaansehz <javaansehz@gmail.com>
2019-03-05 05:58:27 +0100

    beispiele

---
+++ docs/includes/slides/02-minikube/04-beispiel.js
@@ -0,0 +1,8 @@
+var canvasBeispielMinikube1 = document.getElementById('canvasBeispielMinikube1');
+rectangle(canvasBeispielMinikube1, 110, 50, 280, 80, burgundy, 'Angular', 'black');
+rectangle(canvasBeispielMinikube1, 110, 10, 120, 40, burgundy, 'Frontend', 'black');
+
+rectangle(canvasBeispielMinikube1, 110, 230, 280, 80, blue, 'Spring Boot', 'black');
+rectangle(canvasBeispielMinikube1, 110, 190, 120, 40, blue, 'Backend', 'black');
+
+ellipse(canvasBeispielMinikube1, 290, 290, 120, 80, green, 'H2 DB', 'black');
---
+++ docs/includes/slides/03-helm/04-beispiel.js
@@ -0,0 +1,9 @@
+var canvasBeispielHelm1 = document.getElementById('canvasBeispielHelm1');
+rectangle(canvasBeispielHelm1, 110, 50, 280, 80, burgundy, 'Angular', 'black');
+rectangle(canvasBeispielHelm1, 110, 10, 120, 40, burgundy, 'Frontend', 'black');
+
+rectangle(canvasBeispielHelm1, 110, 230, 280, 80, blue, 'Spring Boot', 'black');
+rectangle(canvasBeispielHelm1, 110, 190, 120, 40, blue, 'Backend', 'black');
+
+rectangle(canvasBeispielHelm1, 110, 410, 280, 80, green, 'Postgres', 'black');
+rectangle(canvasBeispielHelm1, 110, 370, 120, 40, green, 'Database', 'black');
